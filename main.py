d478fdd41f65a2aeb9507242fde120f430074235
J. Stojanovic <123444692+aresssera@users.noreply.github.com>
2023-06-13 13:19:52 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pandas as pd
+import re
 from urllib.request import Request, urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
 url = 'https://www.uvek-gis.admin.ch/BFE/ogd/staging/'
 
 def read_url(url):
+  
+  csvList = []
+  url = url.replace(" ","%20")
+  req = Request(url)
+  a = urlopen(req).read()
+  soup = BeautifulSoup(a, 'html.parser')
+  x = (soup.find_all('a'))
+  
+  for elem in x:
+    stringElem = str(elem)
 
-    csvList = []
+    if 'csv' in stringElem:
+      csvList.append(re.search('href="(.*)"', stringElem).group(1))
 
-    url = url.replace(" ","%20")
-    req = Request(url)
-    a = urlopen(req).read()
-    soup = BeautifulSoup(a, 'html.parser')
-    x = (soup.find_all('a'))
-    for i in x:
-        file_name = i.extract().get_text()
-        if '.csv' in file_name:
-          csvList.append(file_name)
-
-    df = pd.DataFrame(csvList, columns= ['fileNames'])
-    df.to_csv('files/fileNames.csv')
+  df = pd.DataFrame(csvList, columns= ['fileNames'])
+  df.to_csv('files/fileNames.csv')
 
 read_url(url)
