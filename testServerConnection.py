124ab7f8b0ebec5ad74f46f82312f6995b1f399d
Jana <123444692+aresssera@users.noreply.github.com>
2023-07-21 10:42:41 +0200

    set passive mode
    should already be by default

--- testServerConnection.py
+++ testServerConnection.py
@@ -5,6 +5,10 @@ ftp_host = '10.73.220.213'
 ftp_port = 21
 
 ftp = FTP_TLS()
+
+# set passive mode
+ftp.set_pasv(True)
+
 ftp.set_pasv(True)
 ftp.connect(ftp_host, ftp_port)
 ftp.login(os.environ.get(ACCOUNT_NAME), os.environ.get(KEY_PHRASE))
