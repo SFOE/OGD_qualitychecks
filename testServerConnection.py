e5aa0bdc38098d75123000d97af400af39965368
Jana <123444692+aresssera@users.noreply.github.com>
2023-07-21 10:48:04 +0200

    delay connection
    the FTP server might need some time before accepting incoming connections

--- testServerConnection.py
+++ testServerConnection.py
@@ -1,4 +1,5 @@
 import os
+import time
 from ftplib import FTP_TLS
 
 ftp_host = '10.73.220.213'
@@ -9,7 +10,9 @@ ftp = FTP_TLS()
 # set passive mode
 ftp.set_pasv(True)
 
-ftp.set_pasv(True)
+# sleep a few seconds before connecting
+time.sleep(5)
+
 ftp.connect(ftp_host, ftp_port)
 ftp.login(os.environ.get(ACCOUNT_NAME), os.environ.get(KEY_PHRASE))
 
